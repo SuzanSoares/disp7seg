57fd80c0790afd2836620b03148f3d20f5b3c6e8
19267692 <19267692@DSN1271158612.cfp127aluno.local>
2021-02-25 15:39:13 -0300

    Situação inicial do programa

---
+++ disp7seg.X/main.c
@@ -0,0 +1,16 @@
+/*
+ * File:   main.c
+ * Author: 19267692
+ *
+ * Created on 25 de Fevereiro de 2021, 14:51
+ */
+
+
+#include <xc.h>
+#include "config.h"
+#include "delay.h"
+
+void main(void) 
+{
+    
+}
